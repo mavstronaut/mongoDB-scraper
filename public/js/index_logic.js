97ea54ac7d01eac66fef862192fe59d67c0fe79e
mavstronaut <mavbarona@gmail.com>
2019-08-19 15:45:44 -0400

    adding article jquery/ajax loop

--- public/js/index_logic.js
+++ public/js/index_logic.js
@@ -12,3 +12,49 @@ function carousel() {
     setTimeout(carousel, 2000);
 };
 
+$("#scrapeButton").on("click", function() {
+    $.ajax({
+        type:"GET",
+        url:"/articles"
+    }).then(function(response) {
+        console.log(response);
+
+        const articleResults = $("#results");
+        articleResults.empty();
+
+        for (i=0; i < response.length; i++) {
+            const article = response[i];
+            
+            const saveButton = $("<button>")
+                .addClass("saveButton")
+                .text("Save")
+                .attr("id", article._id);
+
+            const title = $("<div>")
+                .addClass("title")
+                .text(article.title)
+                .append(saveButton);
+            
+            const link = $("<a>")
+                .addClass("link")
+                .text(article.link)
+                .attr("href", article.link)
+                .attr("target", "_blank");
+            
+            const summary = $("<p>")
+                .addClass("summary")
+                .text(article.summary);
+                
+            const listItem = $("<li>")
+                .addClass("article")
+                .append(title, link, summary);
+            
+                articleResults.append(listItem);
+        }
+    });
+
+    hideContainer();
+
+    showScrapeResults();
+
+});
